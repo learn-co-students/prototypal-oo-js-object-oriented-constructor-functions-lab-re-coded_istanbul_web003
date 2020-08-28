b4b1894ede196127b9781662cd162ebd8325406c
fadime94 <fadime.ozdemir11@gmail.com>
2020-08-28 12:04:03 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+/* + Scooter with `year`, `color`, and `model` properties
++ Driver with `name`, `age`, and `experience` properties
++ PickupLocation with `address` and `city` properties
+ */
+
+
+ function Scooter(year, color, model ){
+     this.year = year;
+     this.color = color;
+     this.model = model;
+ }
+
+ function Driver(name, age, experience){
+     this.name = name;
+     this.age = age;
+     this.experience = experience;
+ }
+
+
+ function PickupLocation(address, city){
+    this.address = address;
+    this.city = city;
+  }
